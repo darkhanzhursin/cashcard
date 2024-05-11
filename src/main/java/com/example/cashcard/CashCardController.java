b8dd990c979f15c304d4c11975e4eb3a8464e99b
Darkhan Zhursin <daratechn@gmail.com>
2024-05-10 20:28:27 -0500

    TDD REST GET method

---
+++ src/main/java/com/example/cashcard/CashCardController.java
@@ -0,0 +1,22 @@
+package com.example.cashcard;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cashcards")
+public class CashCardController {
+
+    @GetMapping("/{requestId}")
+    private ResponseEntity<CashCard> findById(@PathVariable Long requestId) {
+        if (requestId.equals(99L)) {
+            CashCard cashCard = new CashCard(99L, 123.45);
+            return ResponseEntity.ok(cashCard);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
