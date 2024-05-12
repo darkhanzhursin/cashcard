2bcaac86ea30ef9788849dd8cd06f92673f9c638
Darkhan Zhursin <daratechn@gmail.com>
2024-05-12 06:54:16 -0500

    Spring Data using by H2

--- src/main/java/com/example/cashcard/CashCardController.java
+++ src/main/java/com/example/cashcard/CashCardController.java
@@ -6,15 +6,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/cashcards")
 public class CashCardController {
 
+    private final CashCardRepository cashCardRepository;
+
+    private CashCardController(CashCardRepository cashCardRepository) {
+        this.cashCardRepository = cashCardRepository;
+    }
+
     @GetMapping("/{requestId}")
     private ResponseEntity<CashCard> findById(@PathVariable Long requestId) {
-        if (requestId.equals(99L)) {
-            CashCard cashCard = new CashCard(99L, 123.45);
-            return ResponseEntity.ok(cashCard);
+        Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestId);
+        if (cashCardOptional.isPresent()) {
+            return ResponseEntity.ok(cashCardOptional.get());
         } else {
             return ResponseEntity.notFound().build();
         }
---
+++ src/main/java/com/example/cashcard/CashCardRepository.java
@@ -0,0 +1,6 @@
+package com.example.cashcard;
+
+import org.springframework.data.repository.CrudRepository;
+
+interface CashCardRepository extends CrudRepository<CashCard, Long> {
+}
